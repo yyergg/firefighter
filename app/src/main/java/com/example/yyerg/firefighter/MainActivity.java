659c3306e41995112742b218eb4f6b3d8da9b921
HuangChung-Hao重豪 <HuangChung-Hao>
2016-03-13 08:23:09 +0800

    http post

--- app/src/main/java/com/example/yyerg/firefighter/MainActivity.java
+++ app/src/main/java/com/example/yyerg/firefighter/MainActivity.java
@@ -30,12 +30,6 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -45,6 +39,23 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+
+import org.apache.http.NameValuePair;
+
+import org.apache.http.client.ClientProtocolException;
+
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+
+import org.apache.http.client.methods.HttpPost;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.apache.http.message.BasicNameValuePair;
+
+import org.apache.http.protocol.HTTP;
+
+import org.apache.http.util.EntityUtils;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     final String APP_TAG = "Firefighter";
@@ -162,21 +173,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     };
 
     private void sendPostDataToInternet() {
-        CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+        HttpPost httpRequest = new HttpPost("http://chasewind.co/yyergg/test.php");
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
         try {
-            URL url = new URL("http://chasewind.co/yyergg/test.php");
-            String sql = URLEncoder.encode("username", "UTF-8")
-                    + "=" + URLEncoder.encode("yyergg", "UTF-8");
-            sql += "&" + URLEncoder.encode("type", "UTF-8")
-                    + "=" + URLEncoder.encode("heartrate", "UTF-8");
-            sql += "&" + URLEncoder.encode("value", "UTF-8")
-                    + "=" + URLEncoder.encode("133", "UTF-8");
 
-            URLConnection conn = url.openConnection();
-            conn.setDoOutput(true);
-            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write(sql);
-            Log.d(APP_TAG, "finish "+sql);
+            httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
+            if (httpResponse.getStatusLine().getStatusCode() == 200)
+
+            {
+                String strResult = EntityUtils.toString(httpResponse.getEntity());
+                Log.d(APP_TAG,strResult);
+
+            }
+//            URL url = new URL("http://chasewind.co/yyergg/test.php");
+//            String sql = URLEncoder.encode("username", "UTF-8")
+//                    + "=" + URLEncoder.encode("yyergg", "UTF-8");
+//            sql += "&" + URLEncoder.encode("type", "UTF-8")
+//                    + "=" + URLEncoder.encode("heartrate", "UTF-8");
+//            sql += "&" + URLEncoder.encode("value", "UTF-8")
+//                    + "=" + URLEncoder.encode("133", "UTF-8");
+
+//            URLConnection conn = url.openConnection();
+//            conn.setDoOutput(true);
+//            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+//            wr.write(sql);
         }catch(Exception e){
             Log.d(APP_TAG,e.toString());
         }
